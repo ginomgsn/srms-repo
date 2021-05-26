918ea6e28a7e745e985a8d8630ad815074f23b0d
Gino Ben Magsino <ginomgsn05@gmail.com>
2021-05-27 01:03:32 +0800

    Main.java : Created Subject.java : Created Student.java : Modified Features

--- Student Record Management System/src/com/srms/Student.java
+++ Student Record Management System/src/com/srms/Student.java
@@ -1,7 +1,53 @@
 package com.srms;
 
+import java.util.HashMap;
+
 public class Student {
-	String name;
-	String[] subjects;
-	double[] grades;
+	private String name;
+	private String section;
+
+	private Subject[] subjects;
+	private HashMap<Subject, Double> grades;
+	
+	public Student(String name, String section, Subject[] subjects,
+			HashMap<String, String> schedules) {
+		super();
+		this.name = name;
+		this.section = section;
+		this.subjects = subjects;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getSection() {
+		return section;
+	}
+	
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public Subject[] getSubjects() {
+		return subjects;
+	}
+
+	public HashMap<Subject, Double> getGrades() {
+		return grades;
+	}
+
+	public void setSubjects(Subject[] subjects) {
+		this.subjects = subjects;
+	}
+
+	public void setGrades(HashMap<Subject, Double> grades) {
+		this.grades = grades;
+	}
+	
+	
 }
